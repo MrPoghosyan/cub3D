562addd2752d96619860b23de707ee2fc81a4ca3
Natalie Harutyunyan <nataharutyunyan6@gmail.com>
2026-01-20 01:44:43 +0400

    validation & pars

--- src/validation_helpers.c
+++ src/validation_helpers.c
@@ -1,8 +1,7 @@
 #include "cub3D.h"
 
-// Վերադարձնում է նիշը քարտեզի դիրքում (y,x),վերադարձնում է բացատ եթե դուրս է սահմաններից 
-static int get_char_at(t_map *map,
-	int y, int x)
+// Վերադարձնում է նիշը քարտեզի դիրքում (y,x),վերադարձնում է բացատ եթե դուրս է սահմաններից
+static int	get_char_at(t_map *map, int y, int x)
 {
 	if (!map || !map->grid)
 		return (' ');
@@ -85,67 +84,67 @@ int	parse_rgb(char *color_str, int *r, int *g, int *b)
 	return (1);
 }
 
-// Ստուգում է արդյոք նիշը վավեր է քարտեզում (բացատ, պատ, հատակ,խաղացողի ուղղություններ)
+// Ստուգում է արդյոք նիշը վավեր է քարտեզում (բացատ, պատ,հատակ,խաղացողի ուղղություններ)
 int	is_map_char(char c)
+{
+	if (c == ' ' || c == '0' || c == '1' || c == 'N' || c == 'S' || c == 'E'
+		|| c == 'W')
+		return (1);
+	return (0);
+}
+
+// Ստուգում է արդյոք նիշը պետք է շրջապատվի պատերով (հատակներ և խաղացողներ)
+int	needs_enclosure(char c)
+{
+	if (c == '0' || c == 'N' || c == 'S' || c == 'E' || c == 'W')
+		return (1);
+	return (0);
+}
+
+// Ստուգում է,որ քարտեզի(y,x)արգումենտները պատշաճ փակված են պատերով բոլոր կողմերից
+int	cell_enclosed(t_map *map, int y, int x)
+{
+	if (!map || !map->grid)
+		return (0);
+	if (y == 0 || x == 0 || y == map->height - 1 || x == map->width - 1)
+		return (0);
+	if (get_char_at(map, y - 1, x) == ' ' || get_char_at(map, y + 1, x) == ' '
+		|| get_char_at(map, y, x - 1) == ' ' || get_char_at(map, y, x
+			+ 1) == ' ')
+		return (0);
+	return (1);
+}
+
+// Մշակում է մեկ քարտեզի բջիջ. վավերացնում է նիշը,
+// հաշվում է խաղացողներին և ստուգում շրջափակումը
+int	process_cell(t_map *map, int y, int x, int *player_count)
+{
+	char	c;
+
+	c = get_char_at(map, y, x);
+	if (c == '\0')
+		c = ' ';
+	if (!is_map_char(c))
 	{
-		if (c == ' ' || c == '0' || c == '1' || c == 'N' || c == 'S' || c == 'E'
-			|| c == 'W')
-			return (1);
+		fprintf(stderr, "Error\nInvalid map character '%c' at (%d,%d)\n", c, y,
+			x);
 		return (0);
 	}
-
-	// Ստուգում է արդյոք նիշը պետք է շրջապատվի պատերով (հատակներ և խաղացողներ)
-	int needs_enclosure(char c)
+	if (c == 'N' || c == 'S' || c == 'E' || c == 'W')
 	{
-		if (c == '0' || c == 'N' || c == 'S' || c == 'E' || c == 'W')
-			return (1);
-		return (0);
+		(*player_count)++;
+		map->player_x = x;
+		map->player_y = y;
+		map->player_dir = c;
 	}
-
-	// Ստուգում է,որ քարտեզի(y,x)արգումենտները պատշաճ փակված են պատերով բոլոր կողմերից
-	int cell_enclosed(t_map *map, int y, int x)
+	if (needs_enclosure(c))
 	{
-		if (!map || !map->grid)
-			return (0);
-		if (y == 0 || x == 0 || y == map->height - 1 || x == map->width - 1)
-			return (0);
-		if (get_char_at(map, y - 1, x) == ' ' || get_char_at(map, y + 1,
-				x) == ' ' || get_char_at(map, y, x - 1) == ' '
-			|| get_char_at(map, y, x + 1) == ' ')
-			return (0);
-		return (1);
-	}
-
-	// Մշակում է մեկ քարտեզի բջիջ. վավերացնում է նիշը,
-	// հաշվում է խաղացողներին և ստուգում շրջափակումը
-	int process_cell(t_map *map, int y, int x, int *player_count)
-	{
-		char c;
-
-		c = get_char_at(map, y, x);
-		if (c == '\0')
-			c = ' ';
-		if (!is_map_char(c))
+		if (!cell_enclosed(map, y, x))
 		{
-			fprintf(stderr, "Error\nInvalid map character '%c' at (%d,%d)\n", c,
-				y, x);
+			fprintf(stderr, "Error\nMap cell at (%d,%d) is not enclosed\n", y,
+				x);
 			return (0);
 		}
-		if (c == 'N' || c == 'S' || c == 'E' || c == 'W')
-		{
-			(*player_count)++;
-			map->player_x = x;
-			map->player_y = y;
-			map->player_dir = c;
-		}
-		if (needs_enclosure(c))
-		{
-			if (!cell_enclosed(map, y, x))
-			{
-				fprintf(stderr, "Error\nMap cell at (%d,%d) is not enclosed\n",
-					y, x);
-				return (0);
-			}
-		}
-		return (1);
 	}
+	return (1);
+}
